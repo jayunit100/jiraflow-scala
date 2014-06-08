2c7d468598119df6c03bf74752615eb72e147c77
jayunit100 <jayunit100@gmail.com>
2014-06-08 02:05:19 -0400

    working example

--- src/main/scala/org/apache/bigtop/devtools/JIRAExample.scala
+++ src/main/scala/org/apache/bigtop/devtools/JIRAExample.scala
@@ -1,7 +1,23 @@
 package org.apache.bigtop.devtools
 
-class JIRAExample {
+import net.rcarz.jiraclient.Issue
+import net.rcarz.jiraclient.JiraException
+import net.rcarz.jiraclient.JiraClient
+import net.rcarz.jiraclient.BasicCredentials
+import scala.collection.JavaConversions._
 
+object JIRAExample extends App {
+
+  val creds = new BasicCredentials("jayunit100", "x");
+  val client = new JiraClient("https://issues.apache.org/jira", creds);
+  val issueResult = 
+    client.
+      searchIssues("project=BIGTOP AND status=OPEN AND updated > -320m");
   
+  println(issueResult.total);
   
+  issueResult.
+      issues.
+         foreach((i: Issue) => println(i));
+
 }
